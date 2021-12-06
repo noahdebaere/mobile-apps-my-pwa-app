c7efae1c708ba93bed5c1637dea6a56e2e0924c9
Noah Debaere <noah.debaere@gmail.com>
2021-12-06 11:37:36 +0100

    HelloWorld deleted completely

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,10 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   },
   created() {
     document.addEventListener(
