5d19ddf02c3395a504e8436beaaf150f5e4e187b
Noah Debaere <noah.debaere@gmail.com>
2021-12-06 11:58:27 +0100

    another small change to trigger update button

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <h1> This is my first ever PWA Application </h1>
     <img alt="Vue logo" src="./assets/logo.png">
     <button  v-if="updateAvailable" @click="update">Click to update application</button>
-    <p> This site is created by Noah Debaere for the Mobile Apps course</p>
+    <p> This site is created by Noah Debaere for the Mobile Apps course!</p>
   </div>
 </template>
 
